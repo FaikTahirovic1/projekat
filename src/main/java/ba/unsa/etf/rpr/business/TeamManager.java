8831cef510adc5553df901f7c26692184c7537e1
FaikTahirovic1 <ftahirovic1@etf.unsa.ba>
2022-12-22 18:00:39 +0100

    teamManager

---
+++ src/main/java/ba/unsa/etf/rpr/business/TeamManager.java
@@ -0,0 +1,20 @@
+package ba.unsa.etf.rpr.business;
+
+import ba.unsa.etf.rpr.Exception.F1Exception;
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.Driver;
+import ba.unsa.etf.rpr.domain.Team;
+
+import java.util.List;
+
+public class TeamManager {
+    public List<Team> getAll() throws F1Exception {
+        return DaoFactory.teamDao().getAll();
+    }
+    public List<Team> searchTeams(String text) throws F1Exception {
+        return DaoFactory.teamDao().searchByText(text);
+    }
+    public void delete(int id) throws F1Exception{
+        DaoFactory.teamDao().delete(id);
+    }
+}
