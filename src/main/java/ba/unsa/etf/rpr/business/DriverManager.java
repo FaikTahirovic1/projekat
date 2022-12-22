e26cb00373d2141a9862e269b2bfb4d5f4c5902e
FaikTahirovic1 <ftahirovic1@etf.unsa.ba>
2022-12-22 17:57:06 +0100

    driver manager

---
+++ src/main/java/ba/unsa/etf/rpr/business/DriverManager.java
@@ -0,0 +1,22 @@
+package ba.unsa.etf.rpr.business;
+
+import ba.unsa.etf.rpr.Exception.F1Exception;
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.Driver;
+import ba.unsa.etf.rpr.dao.DaoFactory;
+
+import java.util.List;
+
+public class DriverManager {
+    public List<Driver> getAll() throws F1Exception {
+        return DaoFactory.driverDao().getAll();
+    }
+
+    public List<Driver> searchQuotes(String text) throws F1Exception {
+        return DaoFactory.driverDao().searchByText(text);
+    }
+
+    public void delete(int id) throws F1Exception{
+        DaoFactory.driverDao().delete(id);
+    }
+}
--- src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
@@ -174,4 +174,5 @@ public AbstractDao(String tableName) {
 
 
 
+
 }
