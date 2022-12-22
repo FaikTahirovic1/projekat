7ca8bbc8b20c5626fa005b99e2fdce34cb54feb6
FaikTahirovic1 <ftahirovic1@etf.unsa.ba>
2022-12-22 01:20:58 +0100

    driverdaosql

--- src/main/java/ba/unsa/etf/rpr/dao/DriverDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/DriverDaoSQLImpl.java
@@ -4,6 +4,8 @@ package ba.unsa.etf.rpr.dao;
 import ba.unsa.etf.rpr.domain.Driver;
 
 import java.sql.ResultSet;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class DriverDaoSQLImpl extends AbstractDao<Driver> implements DriverDao{
     public DriverDaoSQLImpl(){
@@ -25,4 +27,14 @@ public class DriverDaoSQLImpl extends AbstractDao<Driver> implements DriverDao{
         }
     }
 
+    public Map<String,Object> object2row(Driver d){
+        Map<String,Object> objekat= new TreeMap<String,Object>();
+        objekat.put("idDrivers",d.getId());
+        objekat.put("Age",d.getAge());
+        objekat.put("Name",d.getName());
+        objekat.put("favouritetrackid",d.getFavouriteTrack().getId());
+        objekat.put("teamid",d.getTeam().getId());
+        return objekat;
+
+    }
 }
