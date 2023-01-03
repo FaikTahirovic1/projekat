3ea1d0ba016593d2f8160d2cbc54c1dc38304eda
FaikTahirovic1 <ftahirovic1@etf.unsa.ba>
2023-01-03 18:14:25 +0100

    refaktorisanje dao

--- src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
@@ -7,23 +7,28 @@ import java.sql.*;
 import java.util.*;
 
 public abstract class AbstractDao <T extends Idable> implements Dao<T>{
-private Connection connection;
+private static Connection connection=null;
 private String tableName;
 
 public AbstractDao(String tableName) {
-    try{
-        this.tableName = tableName;
-        Properties p = new Properties();
-        p.load(ClassLoader.getSystemResource("application.properties").openStream());
-        String url = p.getProperty("db.connection_string");
-        String username = p.getProperty("db.username");
-        String password = p.getProperty("db.password");
-        this.connection = DriverManager.getConnection(url,username,password);
-    }catch(Exception e){
-        System.out.println("Greska pri stupanju konekcije na bazu");
-        e.printStackTrace();
-    }
+    this.tableName = tableName;
+    if(connection==null) createConnection();
 }
+    private static void createConnection(){
+        if(AbstractDao.connection==null) {
+            try {
+                Properties p = new Properties();
+                p.load(ClassLoader.getSystemResource("application.properties").openStream());
+                String url = p.getProperty("db.connection_string");
+                String username = p.getProperty("db.username");
+                String password = p.getProperty("db.password");
+                AbstractDao.connection = DriverManager.getConnection(url, username, password);
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.exit(0);
+            }
+        }
+    }
 
     public Connection getConnection() {
         return connection;
