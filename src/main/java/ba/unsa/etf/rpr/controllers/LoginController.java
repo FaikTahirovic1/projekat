cf7a239c76cba5764aac34e951814ba623530327
FaikTahirovic1 <ftahirovic1@etf.unsa.ba>
2023-01-24 18:57:26 +0100

    dodavanje fx:id radi listenera

--- src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
@@ -1,6 +1,8 @@
 package ba.unsa.etf.rpr.controllers;
 
 import javafx.application.Platform;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -9,19 +11,28 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class LoginController {
-
+    @FXML
+    public TextField loginField;
+    @FXML
+    private String username;
 
     @FXML
-
-    void initialize() {
+    public void initialize() {
 
     }
+
+
+
     public void closeApp(ActionEvent actionEvent){
         Platform.exit();
         System.exit(0);
@@ -44,6 +55,9 @@ public class LoginController {
         }
     }
     public void openHome(ActionEvent actionevent){
+        username = loginField.getText();
+        System.out.println(username);
+
         openDialog("Home","/fxml/HomePage.fxml",new HomeController());
     }
 
