d121b8f88a9b018ebea85fa75db2d8cd2deb9dbe
FaikTahirovic1 <ftahirovic1@etf.unsa.ba>
2023-02-08 22:08:58 +0100

    testiranje managera

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -2,9 +2,12 @@ package ba.unsa.etf.rpr;
 
 import ba.unsa.etf.rpr.Exception.F1Exception;
 import ba.unsa.etf.rpr.business.DriverManager;
+import ba.unsa.etf.rpr.business.TeamManager;
+import ba.unsa.etf.rpr.business.TrackManager;
 import ba.unsa.etf.rpr.dao.Dao;
 import ba.unsa.etf.rpr.domain.Driver;
 import ba.unsa.etf.rpr.domain.Team;
+import ba.unsa.etf.rpr.domain.Track;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,7 +18,17 @@ import java.util.List;
  */
 public class App {
     public static void main(String[] args) throws F1Exception {
-        DriverManager manager = new DriverManager();
-        System.out.println(manager.getAll().size());
+        TeamManager Team = new TeamManager();
+        System.out.println(Team.getAll().size());
+        ArrayList<Team> timovi = (ArrayList<ba.unsa.etf.rpr.domain.Team>) Team.getAll();
+        System.out.println(timovi.get(1).getName());
+
+        TrackManager trm = new TrackManager();
+        System.out.println(trm.getAll().size());
+        ArrayList<Track> staze = (ArrayList<Track>) trm.getAll();
+
+
+
+
     }
 }
