02ed02edf9e83bb2a5dbad661d0c05b66dc62db6
FaikTahirovic1 <ftahirovic1@etf.unsa.ba>
2023-02-09 21:54:37 +0100

    kreiran controller za team screen te kreiran manager privatni

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/TeamScreenController.java
@@ -0,0 +1,7 @@
+package ba.unsa.etf.rpr.controllers;
+
+import ba.unsa.etf.rpr.business.TeamManager;
+
+public class TeamScreenController {
+    private final TeamManager tim = new TeamManager();
+}
