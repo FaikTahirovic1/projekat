e435adbb467f7060887f0d604b48272a689da432
FaikTahirovic1 <ftahirovic1@etf.unsa.ba>
2023-02-09 01:42:42 +0100

    vracanje bez x vec na dugmic, discard new creating za track

--- src/main/java/ba/unsa/etf/rpr/controllers/CreateTrackController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/CreateTrackController.java
@@ -5,9 +5,17 @@ import ba.unsa.etf.rpr.business.TrackManager;
 import ba.unsa.etf.rpr.domain.Time;
 import ba.unsa.etf.rpr.domain.Track;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import static java.lang.Integer.parseInt;
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class CreateTrackController {
     private final TrackManager manager = new TrackManager();
@@ -24,5 +32,24 @@ public class CreateTrackController {
         staza.setCountry(trackCountry.getText());
         staza.setName(trackName.getText());
         manager.add(staza);
+    }
+    public void discard(ActionEvent event){
+        openDialog("Tracks","/fxml/TrackScreen.fxml", new TrackScreenController());
+    }
+    private void openDialog(String title, String file, Object controller){
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(file));
+            loader.setController(controller);
+            Stage stage = new Stage();
+            stage.setScene(new Scene((Parent) loader.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+            stage.setTitle(title);
+            stage.initStyle(StageStyle.UTILITY);
+            stage.show();
+        }catch (Exception e){
+            new Alert(Alert.AlertType.NONE, e.getMessage(), ButtonType.OK).show();
+        }
+    }
+    public void goHome(ActionEvent event){
+        openDialog("Home","/fxml/HomePage.fxml", new HomeController(""));
     }
 }
