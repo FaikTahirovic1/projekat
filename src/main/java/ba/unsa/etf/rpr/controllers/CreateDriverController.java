66ab4082172d051e29180204f1a373c879890564
FaikTahirovic1 <ftahirovic1@etf.unsa.ba>
2023-02-10 18:13:36 +0100

    refresh tracks

--- src/main/java/ba/unsa/etf/rpr/controllers/CreateDriverController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/CreateDriverController.java
@@ -5,12 +5,15 @@ import ba.unsa.etf.rpr.business.DriverManager;
 import ba.unsa.etf.rpr.business.TeamManager;
 import ba.unsa.etf.rpr.business.TrackManager;
 import ba.unsa.etf.rpr.domain.Team;
+import ba.unsa.etf.rpr.domain.Track;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DialogPane;
+import javafx.scene.control.TextField;
 
 import java.awt.*;
 import java.util.ArrayList;
@@ -19,16 +22,19 @@ public class CreateDriverController {
     private DriverManager drivermanager = new DriverManager();
     private TrackManager trackmanager = new TrackManager();
     private TeamManager  teammanager = new TeamManager();
-    @FXML
-    private TextField drivernamefield;
-    @FXML
-    private TextField driveridfield;
-    @FXML
-    private ChoiceBox<String> teamcb;
-    @FXML
-    private ChoiceBox<String> trackcb;
 
-    /*@FXML
+    @FXML
+    public ChoiceBox<String> teamcb;
+    @FXML
+    public ChoiceBox<String> trackcb;
+    @FXML
+    public DialogPane dialogp;
+    @FXML
+    public TextField namefield;
+    @FXML
+    public TextField idfield;
+
+    @FXML
     public void initialize() throws F1Exception {
         ArrayList<Team> timovi = (ArrayList<Team>) teammanager.getAll();
         String teamString[] = new String[timovi.size()];
@@ -37,7 +43,7 @@ public class CreateDriverController {
         }
         teamcb.getItems().addAll(teamString);
         refreshTeams();
-    }*/
+    }
     private void refreshTeams() throws F1Exception{
         try {
             ArrayList<String> teamNames = new ArrayList<>();
@@ -46,6 +52,18 @@ public class CreateDriverController {
                 teamNames.add(timovi.get(i).getName());
             }
             //teamcb.setItems(FXCollections.observableArrayList(teamNames));
+        } catch (F1Exception e) {
+            new Alert(Alert.AlertType.NONE, e.getMessage(), ButtonType.OK).show();
+        }
+    }
+
+    private void refreshTracks(){
+        try{
+            ArrayList<String> trackNames = new ArrayList<>();
+            ArrayList<Track>tracks = (ArrayList<Track>) trackmanager.getAll();
+            for(int i = 0; i < tracks.size(); i++){
+                trackNames.add(tracks.get(i).getName());
+            }
         } catch (F1Exception e) {
             new Alert(Alert.AlertType.NONE, e.getMessage(), ButtonType.OK).show();
         }
