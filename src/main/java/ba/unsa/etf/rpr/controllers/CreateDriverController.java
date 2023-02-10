391a5dd1f1161f1cf30505497f33abf614cafaed
FaikTahirovic1 <ftahirovic1@etf.unsa.ba>
2023-02-10 18:46:56 +0100

    go back

--- src/main/java/ba/unsa/etf/rpr/controllers/CreateDriverController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/CreateDriverController.java
@@ -10,14 +10,21 @@ import ba.unsa.etf.rpr.domain.Track;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DialogPane;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.awt.*;
 import java.util.ArrayList;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class CreateDriverController {
     private DriverManager drivermanager = new DriverManager();
@@ -88,7 +95,22 @@ public class CreateDriverController {
         drivermanager.add(d);
 
     }
-
+    private void openDialog(String title, String file, Object controller){
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(file));
+            loader.setController(controller);
+            Stage stage = new Stage();
+            stage.setScene(new Scene((Parent) loader.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+            stage.setTitle(title);
+            stage.initStyle(StageStyle.UTILITY);
+            stage.show();
+        }catch (Exception e){
+            new Alert(Alert.AlertType.NONE, e.getMessage(), ButtonType.OK).show();
+        }
+    }
+public void back(ActionEvent actionEvent){
+    openDialog("Drivers","/fxml/DriverScreen.fxml", new DriverScreenController());
+}
     private Team findTeamByName(String name) throws F1Exception {
         for(int i = 0; i < teammanager.getAll().size(); i++){
             if(teammanager.getAll().get(i).getName().equals(name))return teammanager.getAll().get(i);
