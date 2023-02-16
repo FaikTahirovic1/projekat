b04f40288b57a2ceea280a7be3fa42c468e58909
FaikTahirovic1 <ftahirovic1@etf.unsa.ba>
2023-02-16 15:00:54 +0100

    add team test

--- src/test/java/ba/unsa/etf/rpr/business/TeamManagerTest.java
+++ src/test/java/ba/unsa/etf/rpr/business/TeamManagerTest.java
@@ -1,13 +1,20 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.Exception.F1Exception;
+import ba.unsa.etf.rpr.dao.DaoFactory;
 import ba.unsa.etf.rpr.dao.TeamDaoSQLImpl;
 import ba.unsa.etf.rpr.domain.Team;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import static org.mockito.Mockito.when;
 
 public class TeamManagerTest {
     private TeamManager teamManager;
@@ -56,6 +63,28 @@ public class TeamManagerTest {
 
     public void setTeamList(List<Team> teamList) {
         this.teamList = teamList;
+    }
+
+    /**
+     * this tests adding a team
+     * @throws F1Exception
+     */
+    @Test
+    public void addTeam() throws F1Exception {
+        MockedStatic<DaoFactory> daoFactoryMockedStatic = Mockito.mockStatic(DaoFactory.class);
+        daoFactoryMockedStatic.when(DaoFactory::teamDao).thenReturn(teamDaoSQLMock);
+        when(DaoFactory.teamDao().getAll()).thenReturn(teamList);
+        when(teamManager.getAll()).thenReturn(teamList);
+
+
+        team = new Team(50, "Some team","Some country");
+        Mockito.doCallRealMethod().when(teamManager).add(team);
+        teamManager.add(team);
+
+        Assertions.assertTrue(true);
+        Mockito.verify(teamManager).add(team);
+        daoFactoryMockedStatic.close();
+
     }
 
 }
